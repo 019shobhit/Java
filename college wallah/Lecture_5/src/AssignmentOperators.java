675c49f68eb582ec3de573a0bc62a4f3a5fd5799
019shobhit <148512420+019shobhit@users.noreply.github.com>
2024-05-05 11:14:03 +0530

    Add files via upload

---
+++ college wallah/Lecture_5/src/AssignmentOperators.java
@@ -0,0 +1,19 @@
+public class AssignmentOperators {
+    public static void main(String[] args) {
+        int p=10,q;
+        q=p;
+        System.out.println(q);
+
+        p+=q;
+        System.out.println(p);
+
+        p-=q;
+        System.out.println(p);
+
+        p/=q;
+        System.out.println(p);
+
+        q%=p;
+        System.out.println(q);
+    }
+}
---
+++ college wallah/Lecture_5/src/Bitwise_operator.java
@@ -0,0 +1,13 @@
+public class Bitwise_operator {
+    public static void main(String[] args) {
+        int p=9,q=10;
+        System.out.println(p|q); //11
+        System.out.println(p&q); //8
+        System.out.println(p^q); //3
+        System.out.println(~p);  //6
+        System.out.println(p<<1);//18
+        System.out.println(p<<2);//36
+        System.out.println(p>>1);//4
+        System.out.println(p>>2);//2
+    }
+}
