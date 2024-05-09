5dafc3ee13d7d4a302cfdb1a33ed3eb12f252d61
Shobhit Chauhan <148512420+019shobhit@users.noreply.github.com>
2024-05-09 23:04:03 +0530

    Add files via upload

---
+++ college wallah/Lecture_9/src/Rectangular_pattern.java
@@ -0,0 +1,10 @@
+public class Rectangular_pattern {
+    public static void main(String[] args) {
+        for(int i=0;i<3;i++){
+            for(int j=0;j<6;j++){
+                System.out.printf("*");
+            }
+            System.out.println("");
+        }
+    }
+}
