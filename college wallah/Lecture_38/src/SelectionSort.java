0787cc6a846e61037ce79a88b09ec4e36e198624
Shobhit Chauhan <148512420+019shobhit@users.noreply.github.com>
2024-06-10 19:33:05 +0530

    Add files via upload

---
+++ college wallah/Lecture_38/src/SelectionSort.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class SelectionSort {
+
+    static void sort(int [] arr){
+        int n =arr.length;
+        for(int i=0;i<n-1;i++){
+            int min_idx=i;
+            for(int j=i+1;j<n;j++){
+                if(arr[j] < arr[min_idx]) {
+                    min_idx = j;
+                }
+            }
+            if(min_idx != i) {
+                int temp = arr[i];
+                arr[i] = arr[min_idx];
+                arr[min_idx] = temp;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of Array: ");
+        int n = sc.nextInt();
+        System.out.print("Enter Elements of Array: ");
+        int[] arr= new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]= sc.nextInt();
+        }
+        System.out.println("sorted Array is: ");
+        sort(arr);
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
