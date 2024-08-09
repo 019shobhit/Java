c63a372ceb95e58960396f9f876b86e2f4d3a5eb
019shobhit <019shobhit@gmail.com>
2024-08-09 23:31:40 +0530

    Upload new Updated code

---
+++ college wallah/Lecture_49/src/StringBasics.java
@@ -0,0 +1,11 @@
+import  java.util.*;
+public class StringBasics {
+    public static void main(String[] args) {
+//        char[] arr = new char[10];
+//        String str = "Shobhit Chauhan";
+        Scanner sc = new Scanner(System.in);
+//        String str = sc.next();
+        String str = sc.nextLine();
+        System.out.println(str);
+    }
+}
