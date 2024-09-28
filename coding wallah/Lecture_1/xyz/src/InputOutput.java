6374a6e37657c438fbab8a4f9b17cfe7fba1030b
019shobhit <019shobhit@gmail.com>
2024-09-28 12:11:07 +0530

    add new codes

---
+++ coding wallah/Lecture_1/xyz/src/InputOutput.java
@@ -0,0 +1,12 @@
+import java.util.Date;
+import java.util.Scanner;
+
+public class InputOutput {
+    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter the character: ");
+//        char ch = sc.next().charAt(3);
+//        System.out.println("Character - "+ch);
+        System.out.println(new Date());
+    }
+}
---
+++ coding wallah/Lecture_1/xyz/src/TypeCasting.java
@@ -0,0 +1,24 @@
+public class TypeCasting {
+    public static void main(String[] args) {
+//        float x = 12.23f;
+//        long y = (long)x;
+//        System.out.println(y);
+
+//        P  -> NP
+//        int a = 2;
+//        String str = String.valueOf(a);
+//        System.out.println(str);
+
+//        NP -> P
+//        String str = "4";
+//        int x = Integer.parseInt(str);
+//        System.out.println(str);
+//        System.out.println(x);
+//        System.out.println((5>2) ? "yes" : "no");
+
+        System.out.println("Shift operators");
+        System.out.println(-500>>>2);
+        System.out.println(100>>>2);
+
+    }
+}
