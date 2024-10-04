f5dd187832bbce4aa87492f2eaf7afada48620b3
019shobhit <019shobhit@gmail.com>
2024-10-04 18:55:23 +0530

    add new codes

---
+++ Coding Wallah/Lecture_5/src/Newtest.java
@@ -0,0 +1,5 @@
+public class Newtest {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Coding Wallah/Lecture_5/src/Test.java
@@ -0,0 +1,19 @@
+
+class Employee{
+    private int id=1 ;
+    private String name = "Shobhit";
+    private int incentive = 500;
+    private int salary = 20000;
+    private void totalSalary(){
+        System.out.println(salary+incentive);
+    }
+}
+
+public class Test {
+    public static void main(String[] args) {
+//        Employee em = new Employee();
+//        em.totalSalary();
+//        System.out.println(Employee.id);
+//        Employee.totalSalary();
+    }
+}
