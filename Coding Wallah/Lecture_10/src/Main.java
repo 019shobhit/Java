7535e806caa0950dd5ce43adf53c6d1b9e6377b4
019shobhit <019shobhit@gmail.com>
2024-10-15 15:03:54 +0530

    push new codes

--- Coding Wallah/Lecture_10/src/Main.java
+++ Coding Wallah/Lecture_10/src/Main.java
@@ -1,10 +1,18 @@
-import java.io.File;
+import java.util.Arrays;
 
-public class Main{
-    public static void main(String[] args) throws  Exception {
-        System.out.println("File Handling in Java");
-        File file = new File("abc.txt");
-        file.createNewFile();
-        System.out.println(file.exists());
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {2,5,15,96,86};
+        int n = arr.length;
+        Arrays.sort(arr);
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                String s1 = String.valueOf(arr[i]);
+                String s2 = String.valueOf(arr[j]);
+                if((s1+s2)>(s2+s1)){
+
+                }
+            }
+        }
     }
 }
---
+++ Coding Wallah/Lecture_15/src/Main.java
@@ -0,0 +1,10 @@
+public class Main{
+    public static void main(String[] args){
+        String url = "";
+        String username = "shobhit2002";
+        String password = "shobhit2002";
+//        try{
+//
+//        }
+    }
+}
