b3aa24973bd38ccaa060b992ffe6ff5574f34e39
019shobhit <019shobhit@gmail.com>
2024-10-21 19:36:47 +0530

    add new codes

---
+++ Coding Wallah/JavaProjectUsingJakarta/src/main/java/myPackage/ChildServlet.java
@@ -0,0 +1,7 @@
+package myPackage;
+
+import jakarta.servlet.http.HttpServlet;
+
+public class ChildServlet extends HttpServlet {
+
+}
--- Coding Wallah/Lecture_15/src/Main.java
+++ Coding Wallah/Lecture_15/src/Main.java
@@ -1,10 +1,5 @@
 public class Main{
     public static void main(String[] args){
-        String url = "";
-        String username = "shobhit2002";
-        String password = "shobhit2002";
-//        try{
-//
-//        }
+        System.out.println("hello world");
     }
 }
