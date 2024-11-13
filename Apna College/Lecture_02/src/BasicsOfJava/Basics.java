184abd431d0a8563f2c74a31ce1bb81cadf44070
019shobhit <019shobhit@gmail.com>
2024-11-13 21:31:56 +0530

    add new codes

--- Apna College/Lecture_02/src/BasicsOfJava/Basics.java
+++ Apna College/Lecture_02/src/BasicsOfJava/Basics.java
@@ -2,8 +2,14 @@ package BasicsOfJava;
 
 public class Basics {
     public static void main(String[] args){
-        System.out.print("Hello world");
-        System.out.print("Hello world");
+//        System.out.print("Hello world");
+//        System.out.print("Hello world");
+//        float a=5f;
+//        float b = 2f;
+//        System.out.println();
+//        System.out.println(a/b);
+        int n = 4+2%+8;
+        System.out.println(n);
 
     }
 }
