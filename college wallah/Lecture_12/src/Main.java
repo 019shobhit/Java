c1999273bfe89f6d59255f9b489d9d7229e0ebf7
019shobhit <019shobhit@gmail.com>
2024-11-16 17:54:04 +0530

    add new codes

--- college wallah/Lecture_12/src/Main.java
+++ college wallah/Lecture_12/src/Main.java
@@ -1,13 +1,29 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 //        float n = 45.49f;
 //        double x = Math.sqrt(10);
 //        System.out.println(Math.sqrt(10));
 //        System.out.println(Math.round(n));
-        int arr[];
-        arr = new int[5];
-        for(int i: arr){
-            System.out.println(i);
+        int[] arr= {1,2,3};
+//        clone
+//        copyOf
+//        copyOfRange
+
+        int[] arr2 = Arrays.copyOf(arr,arr.length);
+        arr2[0] = 0;
+        for(int i : arr){
+            System.out.printf("%d ",i);
         }
+        System.out.println();
+        for(int i : arr2){
+            System.out.printf("%d ",i);
+        }
+//        for(int[] j : arr) {
+//            for (int i : j) {
+//                System.out.println(i);
+//            }
+//        }
     }
 }
