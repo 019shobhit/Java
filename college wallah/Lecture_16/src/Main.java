324406b7d2d506c738635adb4419a2a6628a5bbe
019shobhit <019shobhit@gmail.com>
2024-11-16 21:46:15 +0530

    add new code

--- college wallah/Lecture_16/src/Main.java
+++ college wallah/Lecture_16/src/Main.java
@@ -1,15 +1,17 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class Main{
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int[] arr = {1,4,7,8,5,2};
+        int max1 = Integer.MIN_VALUE;
+        int max2 = Integer.MIN_VALUE;
+        for(int i=0;i<arr.length;i++){
+            if(max1 <arr[i]) max1=arr[i];
+//            if(max2<arr[i] && max1!=arr[i]) max2 = arr[i];
+        }for(int i=0;i<arr.length;i++){
+//            if(max1 <arr[i]) max1=arr[i];
+            if(max2<arr[i] && max1!=arr[i]) max2 = arr[i];
         }
+        System.out.println(max1);
+        System.out.println(max2);
+
     }
 }
