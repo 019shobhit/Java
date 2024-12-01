9e366247f820fd39661d38e34c935bc0d50090d8
019shobhit <019shobhit@gmail.com>
2024-12-01 19:40:21 +0530

    practice dsa

---
+++ college wallah/Lecture_28/src/practice/Fibonacci.java
@@ -0,0 +1,12 @@
+package practice;
+
+public class Fibonacci {
+    static int  fib(int n){
+        if(n==0||n==1) return 1;
+        return fib(n-1)+fib(n-2);
+    }
+    public static void main(String[] args) {
+        int n =10;
+        System.out.println(fib(n));
+    }
+}
