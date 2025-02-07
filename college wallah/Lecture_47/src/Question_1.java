557e5377f5061792f0590dcf6535bd489cc9f9a1
019shobhit <019shobhit@gmail.com>
2025-02-07 11:54:57 +0530

    add

--- college wallah/Lecture_47/src/Question_1.java
+++ college wallah/Lecture_47/src/Question_1.java
@@ -1,8 +1,5 @@
-// Search the 'target' value in 2d integer matrix of dimension n X m and return true if found, else return false.
-// This matrix has the following properties :
-// 1. Integer in each row is sorted from left to right.
-// 2. The first integer of each row is greater than the last integer of the previous row.
-
 public class Question_1 {
-
+    public static void main(String[] args) {
+        
+    }
 }
