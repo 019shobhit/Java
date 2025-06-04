b500d9c4a8e86b026c1a876761f2e7dc6a459f9f
019shobhit@gmail.com <019shobhit@gmail.com>
2025-06-05 01:44:59 +0530

    add new files

--- Apna College/Lecture_02/src/BasicsOfJava/Basics.java
+++ Apna College/Lecture_02/src/BasicsOfJava/Basics.java
@@ -1,15 +1,20 @@
 package BasicsOfJava;
+import java.util.*;
 
 public class Basics {
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        String n1 = sc.nextLec();
 //        System.out.print("Hello world");
 //        System.out.print("Hello world");
 //        float a=5f;
 //        float b = 2f;
 //        System.out.println();
-//        System.out.println(a/b);
-        int n = 4+2%+8;
-        System.out.println(n);
+//        System.out.println(a/b);i
+//        int n = 4+2%+8;
+        System.out.println(name);
+        System.out.println(n1);
 
     }
 }
